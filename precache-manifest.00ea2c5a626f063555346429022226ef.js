075cb0a6ba860c9f52e2d08eb379001c5d702c03
chri911 <delightsome@bigmir.net>
2021-03-09 15:08:54 +0200

    Updates

---
+++ precache-manifest.00ea2c5a626f063555346429022226ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6774853a0a1521bc1fe8811a57d6639f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b86578ca32028a492033",
+    "url": "./static/css/main.60349cba.chunk.css"
+  },
+  {
+    "revision": "1ae3661e6638562f634e",
+    "url": "./static/js/2.47bff1d9.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.47bff1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86578ca32028a492033",
+    "url": "./static/js/main.0e7069ae.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
